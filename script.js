4d43c2a72121b2e2e4848960a38eed2fbfc9c094
Christopher Jones <christopherjones72521@gmail.com>
2016-08-25 13:36:23 -0400

    Nothing fancy

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 $('#clicky').click(function(){
 	$.ajax("infoo.txt").done(function(data){
-		alert(data);
+		$("contents").html(data);
 	}).fail(function(){
 		alert("Could not retrieve data");
 	})
