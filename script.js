b50ee7263d2233e24feb2b93db2edc954dbb2bb7
Christopher Jones <christopherjones72521@gmail.com>
2016-08-25 13:41:48 -0400

    Moar

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 $("#clicky").click(function(){
 	$.ajax("infoo.txt").done(function(data){
-		$("#contents").html(data);
+		// $("#contents").html(data);
+		alert(data);
 	}).fail(function(){
 		alert("Could not retrieve data");
 	})
