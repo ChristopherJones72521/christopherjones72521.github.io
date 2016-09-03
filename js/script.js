3506819bddb544de8e8dbe7ce9c4effee9ebe178
Christopher Jones <christopherjones72521@gmail.com>
2016-09-02 22:44:23 -0400

    More detailed descriptions included

--- js/script.js
+++ js/script.js
@@ -129,23 +129,44 @@ if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == t
 } 
 
 function resultDescription(){
+	var cursedDesc = "If you clicked one button the entire time you would have done better. You don't just have bad luck, you're cursed. However, you were able to consistently predict the wrong answers! Just do the opposite of what you think you should do!";
+	var unluckyDesc = "Remeber that chain letter you broke in the 7th grade? WELL THIS IS WHAT HAPPENS TO PEOPLE WHO DON'T FOLLOW THROUGH! Write those letters and try again.";
+	var averageDesc	= "Congratulations, your mediocre! You've achieved the same odds as a cat walking across your keyboard. Just think, being average means there are lots of other people just like you! Think of all the friends you could have!";
+	var intuitiveDesc =	"You have good instincts! You tend to understand things you can't explain and have no problem trusting your gut. I can't say you'll win the lottery anytime soon. But you might consider the stock market.";
+	var clairvoyantDesc = "Your ability to sense goes beyond empathy. You've enjoyed inexplicable luck all your life and may not have even realized it. Xavier's School for Gifted Youngsters is waiting for you! Dig out that unitard and think of a cool superhero name!";
+	var NostradamusDesc = "You know how the world is going to end, surprise parties aren't a 'thing' for you, and you already know who is going to win the 2016 presidential election (which goes back to my original point). Maybe you CAN know too much?";
+	var MatrixDesc = "You know Kung Fu and are a snappy dresser. But don't forget, the Matrix has you and all of humanity is depending on you. However, if you neglect your duty to humanity and leave us in this simulation, you get to keep your super powers.";
 	var finalPercentile = Math.round(totalChance * 100);
-	console.log(finalPercentile);
 	if((totalChance * 100) <= 25){
-		$('.description').html("You're cursed!");
-	} else if(25 < finalPercentile && finalPercentile <= 50) {
-		$('.description').html("You're unlucky!");
-	} else if(50 < finalPercentile && finalPercentile <= 60) {
-		$('.description').html("You're average!");
-	} else if(60 < finalPercentile && finalPercentile <= 75) {
-		$('.description').html("You're intuitive!");
-	} else if(75 < finalPercentile && finalPercentile <= 85) {
-		$('.description').html("You're clairvoyant!");
-	} else if(85 < finalPercentile && finalPercentile <= 95) {
-		$('.description').html("You're Nostradamus!");
-	} else if(95 < finalPercentile && finalPercentile <= 100) {
-		$('.description').html("Are you from the Matrix?");
-	} else {
+		$('#resultHeading').html("You're cursed!");
+		$('#resultDescription').html(cursedDesc);
+	} 
+	else if(25 < finalPercentile && finalPercentile <= 50) {
+		$('#resultHeading').html("You're unlucky!");
+		$('#resultDescription').html(unluckyDesc);
+	} 
+	else if(50 < finalPercentile && finalPercentile <= 60) {
+		$('#resultHeading').html("You're average!");
+		$('#resultDescription').html(averageDesc);
+	} 
+	else if(60 < finalPercentile && finalPercentile <= 75) {
+		$('#resultHeading').html("You're intuitive!");
+		$('#resultDescription').html(intuitiveDesc);
+	} 
+	else if(75 < finalPercentile && finalPercentile <= 85) {
+		$('#resultHeading').html("You're clairvoyant!");
+		$('#resultDescription').html(clairvoyantDesc);
+	} 
+	else if(85 < finalPercentile && finalPercentile <= 95) {
+		$('#resultHeading').html("You're Nostradamus!");
+		$('#resultDescription').html(NostradamusDesc);
+	} 
+	else if(95 < finalPercentile && finalPercentile <= 100) {
+		$('#resultHeading').html("Are you from the Matrix?");
+		$('#resultDescription').html(MatrixDesc);
+	} 
+	else {
 		alert("There has been an error");
 	}
+	
 }
